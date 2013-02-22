f98ed629b9c34b9a67897d854f077260e58767c1
Jonathan Hartman <jhartman86@gmail.com>
2013-02-21 18:22:39 -0700

    If using this branch and installing with cli_installer, was failing due to Redis dependency being enabled. Fixed now.

--- cli_installer.php
+++ cli_installer.php
@@ -57,6 +57,10 @@
 	## Startup check ##	
 	require($corePath . '/startup/encoding_check.php');
 	
+	## Required for command line installation, with RedisPageCache ##
+	require( DIR_BASE . '/packages/concrete_redis/libraries/concrete_redis.php' );
+	require( DIR_BASE . '/packages/concrete_redis/libraries/page_cache/type/redis.php' );
+	
 	
 	/**
 	 * Custom stuff to work w/ Pagoda
