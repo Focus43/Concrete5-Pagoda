0ec48e5f175cca8b9ebca750f430c7827651a9b6
Jonathan Hartman <jhartman86@gmail.com>
2013-02-23 23:18:55 -0700

    Refactored site.php

--- web/config/site.php
+++ web/config/site.php
@@ -1,11 +1,27 @@
 <?php
-
+	
 	/**
-	 * Is the site running locally? Then create a site.local.php file in the /config folder,
-	 * and DO NOT TRACK IT IN THE REPO. Any team members, or other environments (eg. dev or staging)
-	 * you want to run the site on should have their own site.local.php file.
+	 * PAGODABOX PRODUCTION SETTINGS
 	 */
-	if( !(isset($_SERVER['PAGODA_PRODUCTION']) && $_SERVER['PAGODA_PRODUCTION'] == 'true') ) {
+	if( isset($_SERVER['PAGODA_PRODUCTION']) && ((bool) $_SERVER['PAGODA_PRODUCTION'] === true) ) {
+		
+		// enable all url rewriting
+		define('URL_REWRITING_ALL', true);
+		
+		// needed for successful installation on Pagodabox. see issue
+		// http://www.concrete5.org/developers/bugs/5-6-0-2/install-fails-with-mysql-auto-increment-offset-set/
+		define('REGISTERED_GROUP_ID', '5');
+		define('ADMIN_GROUP_ID', '9');
+	
+	/**
+	 * LOCAL DEVELOPMENT OR STAGING
+	 * 
+	 * Is the site running locally? Then create a site.local.php file in the /config folder,
+	 * and DO NOT TRACK IT IN THE REPO (default settings in .gitignore). Any team members, 
+	 * or other environments (eg. dev or staging) you want to run the site on should have 
+	 * their own site.local.php file.
+	 */
+	}else{
 		
 		require __DIR__ . '/site.local.php';
 		
@@ -15,24 +31,19 @@
 		$_SERVER['DB1_PASS'] = '';
 		$_SERVER['DB1_NAME'] = '';
 		
-		// enable all url rewriting
+		// enable url rewriting. use locally if you have an Apache VirtualHost setup
 		define('URL_REWRITING_ALL', true);
 		*****************************************************************/
 		
-	}else{
-		
-		// enable all url rewriting
-		define('URL_REWRITING_ALL', true);
-		
-		// needed for Pagoda install
-		define('REGISTERED_GROUP_ID', '5');
-		define('ADMIN_GROUP_ID', '9');
-		
 	}
+
 
 	// server variables are set by Pagoda, or by you in site.local.php
 	define('DB_SERVER',     $_SERVER['DB1_HOST']);
-	define('DB_USERNAME',   $_SERVER['DB1_USER']);
-	define('DB_PASSWORD',   $_SERVER['DB1_PASS']);
+    define('DB_USERNAME',   $_SERVER['DB1_USER']);
+    define('DB_PASSWORD',   $_SERVER['DB1_PASS']);
 	define('DB_DATABASE',   $_SERVER['DB1_NAME']);
 	define('PASSWORD_SALT', '6NVukfgwAgqaOi3SMlsWwEqURSe4Xh8pBApvhOauP7blC2kx1FKsHxcjGSXMqP3N');
+	
+	
+	
