951fc1a3ef7e3bc4d9c81dd3955fa77c58875875
Jonathan Hartman <jhartman86@gmail.com>
2013-06-18 17:01:41 -0600

    Reimported cookbooks so not using submodules

---
+++ vagrant_box/cookbooks/vagrant_concrete5/recipes/default.rb
@@ -0,0 +1,22 @@
+# ensure sass is installed
+gem_package 'sass'
+
+# disable default apache vhost
+apache_site "000-default" do
+  enable false
+end
+
+# setup vhost to link to /home/vagrant/app/web
+web_app "default" do
+	server_name 'localhost'
+	docroot "/home/vagrant/app/web"
+	php_timezone 'UTC'
+end
+
+# install node dependencies (uses package.json file in node_grunt)
+execute "npm dependencies installation" do
+	cwd "/home/vagrant/app/build/"
+	user "root"
+	command "/usr/local/bin/npm install; /usr/local/bin/npm install -g grunt-cli"
+	action :run
+end
